c99be64068ee3d0a6857d9ce49565fc3de2c3733
Marciokkkjjj <148603386+Marciokkkjjj@users.noreply.github.com>
2024-06-12 17:32:15 -0300

    MR-HUB.lua

--- MR-HUB.lua
+++ MR-HUB.lua
@@ -1,5 +1,8 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
+local Window = OrionLib:MakeWindow({Name = "MK-HUB", HidePremium = false, SaveConfig = true})
+
+
 local Tab = Window:MakeTab({
 	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
