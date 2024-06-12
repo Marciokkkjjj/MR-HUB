97ec0ce0768a9ac11ce1605efa2ab376d01eb5ac
Marciokkkjjj <148603386+Marciokkkjjj@users.noreply.github.com>
2024-06-12 17:01:44 -0300

    MR-HUB.lua

--- MR-HUB.lua
+++ MR-HUB.lua
@@ -1,1 +1,18 @@
-jbiivi
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/kwargs_Ui_Library/main/source.lua"))()
+
+local UI = Lib:Create{
+   Theme = "Dark", -- or any other theme
+   Size = UDim2.new(0, 555, 0, 400) -- default
+}
+
+local Main = UI:Tab{
+   Name = "Main"
+}
+
+local Divider = Main:Divider{
+   Name = "Main shit"
+}
+
+local QuitDivider = Main:Divider{
+   Name = "Quit"
+}
