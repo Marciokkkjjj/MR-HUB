5f418a910ee637bc5e2646550a191cfbb96a059d
Marciokkkjjj <148603386+Marciokkkjjj@users.noreply.github.com>
2024-06-12 17:29:19 -0300

    MR-HUB.lua

--- MR-HUB.lua
+++ MR-HUB.lua
@@ -1,49 +1,7 @@
-local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/kwargs_Ui_Library/main/source.lua"))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local UI = Lib:Create{
-   Theme = "Dark", -- or any other theme
-   Size = UDim2.new(0, 555, 0, 400) -- default
-}
-
-local Main = UI:Tab{
-   Name = "Main"
-}
-
-local Divider = Main:Divider{
-   Name = "Main shit"
-}
-
-local QuitDivider = Main:Divider{
-   Name = "Quit"
-}
-
---codigos--
-
-local KillAll = Divider:Button{
-   Name = "Kill all",
-   Description = "Kills all the players in the game!",
-   Callback = function()
-       print("All players killed.")
-   end
-}
-
-
-local Quit = QuitDivider:Button{
-   Name = "Closes the ui library.",
-   Callback = function()
-       UI:Quit{
-           Message = "Fuck off...", -- closing message
-           Length = 1 -- seconds the closing message shows for
-       }
-   end
-}
-
-local Quit = QuitDivider:Button{
-   Name = "Closes the ui library.",
-   Callback = function()
-       UI:Main shit{
-           Message = "Fuck off...", -- closing message
-           Length = 1 -- seconds the closing message shows for
-       }
-   end
-}
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
